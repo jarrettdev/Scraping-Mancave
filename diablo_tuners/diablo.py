a1138c485b30f796e0b280a552d649aec8e314d0
Jarrett <moonwet@protonmail.com>
2020-05-11 01:31:29 -0500

    learning bs4

--- diablo_tuners/diablo.py
+++ diablo_tuners/diablo.py
@@ -1,9 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import io
+import json
 
-base_url = 'https://www.marinedepot.com/protein-skimmers'
+base_url = 'https://www.overstock.com/Home-Garden/Mattresses/Twin,/size,/2019/subcat.html?TID=Mattresses:01:Bmod-1:Twin'
 response = requests.get(base_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'})
 
 with io.open('diablo.html','w', encoding = 'utf-8') as html_file:
 	html_file.write(response.text)
+
+content = BeautifulSoup(response.text,'lxml')
+cards = content.findAll('div', {'class':'productCard'})
+
+for card in cards:
+	titleCard = content.findAll('div', {'class':'productCardInfoBarContainerWithTitle'})
